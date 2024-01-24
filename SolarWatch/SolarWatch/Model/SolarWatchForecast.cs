6b7cac6818bc71ce8869eb3e1a4872f5c9c21d07
GaborToth22 <toccs22@gmail.com>
2024-01-24 08:24:57 +0100

    feat: add SolarWatchForecast class

---
+++ SolarWatch/SolarWatch/Model/SolarWatchForecast.cs
@@ -0,0 +1,9 @@
+namespace SolarWatch.Model;
+
+public class SolarWatchForecast
+{
+    public DateTime Date { get; set; }
+    public string SunRise { get; set; }
+    public string SunSet { get; set; }
+    public string City { get; set; }
+}
