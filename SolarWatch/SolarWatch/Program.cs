c1b4fa0a51362759f63cd0b8fff1f2738becf994
GaborToth22 <toccs22@gmail.com>
2024-01-24 08:27:26 +0100

    feat: add the services singletons to program.cs

--- SolarWatch/SolarWatch/Program.cs
+++ SolarWatch/SolarWatch/Program.cs
@@ -1,3 +1,5 @@
+using SolarWatch.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +8,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddSingleton<IGeoCoder, GeoCoder>();
+builder.Services.AddSingleton<ISunDataProvider,SunDataProvider>();
+builder.Services.AddSingleton<IJsonProcessorGeo, JsonProcessorGeo>();
+builder.Services.AddSingleton<IJsonProcessorSun, JsonProcessorSun>();
+    
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
