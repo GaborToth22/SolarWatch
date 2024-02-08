4b31caa0fe3365777b58087528ce8fbc46bfd8a9
GaborToth22 <toccs22@gmail.com>
2024-02-08 12:05:31 +0100

    feat: add Crud operation endpoints to City and SolarWatchForecast (only Admin)

--- SolarWatch/SolarWatch/Controllers/SolarWatchForecastController.cs
+++ SolarWatch/SolarWatch/Controllers/SolarWatchForecastController.cs
@@ -29,7 +29,7 @@ public class SolarWatchForecastController : ControllerBase
         _solarWatchRepository = solarWatchRepository;
     }
 
-    [HttpGet("SolarWatch"), Authorize]
+    [HttpGet("SolarWatch"), Authorize(Roles="User, Admin")]
     public async Task<IActionResult> GetData(string cityName, DateTime date)
     {
         var city = _solarWatchRepository.GetCityByName(cityName);
@@ -47,11 +47,34 @@ public class SolarWatchForecastController : ControllerBase
             solarWatchForecast = await _jsonProcessorSun.ProcessAsync(sunData, city, date);
             _solarWatchRepository.Add(solarWatchForecast);
         }
+        
         try
         {
-
             return Ok(solarWatchForecast);
+        }
 
+        catch (Exception e)
+        {
+            _logger.LogError(e, "An error occurred while processing the data.");
+            return StatusCode(500, "Internal Server Error");
+        }
+    }
+
+    [HttpPost("PostCity"), Authorize(Roles="Admin")]
+    public async Task<IActionResult> PostCity(string name, string country, string state, double lat, double lon)
+    {
+        var city = new City
+        {
+            Name = name,
+            Country = country,
+            State = state,
+            Lat = lat,
+            Lon = lon
+        };
+        _solarWatchRepository.Add(city);
+        try
+        {
+            return Ok(city);
         }
         catch (Exception e)
         {
@@ -60,4 +83,163 @@ public class SolarWatchForecastController : ControllerBase
         }
     }
 
+    [HttpDelete("DeleteCity"), Authorize(Roles = "Admin")]
+    public async Task<ActionResult> DeleteCityById(int id)
+    {
+        var city = _solarWatchRepository.GetCityById(id);
+        if (city != null)
+        {
+            _solarWatchRepository.Delete(city);
+            try
+            {
+                return Ok($"city with Id: {id} deleted");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "An error occurred while processing the data.");
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
+
+        return NotFound($"City with Id: {id} not found");
+    }
+
+    [HttpPatch("UpdateCity"), Authorize(Roles = "Admin")]
+    public async Task<ActionResult> UpdateCity(int id, string? name, string? country, string? state, double? lat, double? lon)
+    {
+        var city = _solarWatchRepository.GetCityById(id);
+        if (city == null)
+        {
+            return NotFound($"City with Id: {id} not found");
+        }
+        
+        if (name != null)
+        {
+            city.Name = name;
+        }
+        if (country != null)
+        {
+            city.Country = country;
+        }
+        if (state != null)
+        {
+            city.State = state;
+        }
+        if (lat != null)
+        {
+            city.Lat = lat.Value;
+        }
+        if (lon != null)
+        {
+            city.Lon = lon.Value;
+        }
+        
+        _solarWatchRepository.Update(city);
+
+        try
+        {
+            return Ok(city);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "An error occurred while processing the data.");
+            return StatusCode(500, "Internal Server Error");
+        }
+    }
+    
+    [HttpDelete("DeleteSolarWatchForecast"), Authorize(Roles = "Admin")]
+    public async Task<ActionResult> DeleteSolarForecastById(int id)
+    {
+        var solarWatchForecast = _solarWatchRepository.GetSolarWatchForecastById(id);
+        if (solarWatchForecast != null)
+        {
+            _solarWatchRepository.Delete(solarWatchForecast);
+            try
+            {
+                return Ok($"solarWatchForecast with Id: {id} deleted");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "An error occurred while processing the data.");
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
+
+        return NotFound($"solarWatchForecast with Id: {id} not found");
+    }
+    
+    [HttpPost("PostSolarWatchForecast"), Authorize(Roles="Admin")]
+    public async Task<IActionResult> PostSolarWatchForecast(DateTime date, string sunset, string sunrise, int cityId)
+    {
+        var city = _solarWatchRepository.GetCityById(cityId);
+        if (city == null)
+        {
+            return NotFound($"City with Id: {cityId} not found");
+        }
+        var solarWatchForecast = new SolarWatchForecast
+        {
+            Date = date,
+            SunSet = sunset,
+            SunRise = sunrise,
+            CityId = cityId
+        };
+        _solarWatchRepository.Add(solarWatchForecast);
+        
+        try
+        {
+            return Ok(solarWatchForecast);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "An error occurred while processing the data.");
+            return StatusCode(500, "Internal Server Error");
+        }
+    }
+    
+    [HttpPatch("UpdateSolarWatchForecast"), Authorize(Roles = "Admin")]
+    public async Task<ActionResult> UpdateSolarWatchForecast(int id, DateTime? date, string? sunRise, string? sunSet, int? cityId)
+    {
+        var solarWatchForecast = _solarWatchRepository.GetSolarWatchForecastById(id);
+        if (solarWatchForecast == null)
+        {
+            return NotFound($"SolarWatchForecast with Id: {id} not found");
+        }
+
+        if (cityId != null)
+        {
+            var city = _solarWatchRepository.GetCityById((int)cityId);
+            if (city == null)
+            {
+                return NotFound($"City with Id: {cityId} not found");
+            }
+
+            solarWatchForecast.CityId = (int)cityId;
+        }
+        
+        if (sunRise != null)
+        {
+            solarWatchForecast.SunRise = sunRise;
+        }
+        if (sunSet != null)
+        {
+            solarWatchForecast.SunSet = sunSet;
+        }
+        if (date != null)
+        {
+            solarWatchForecast.Date = (DateTime)date;
+        }
+        
+        
+        _solarWatchRepository.Update(solarWatchForecast);
+
+        try
+        {
+            return Ok(solarWatchForecast);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "An error occurred while processing the data.");
+            return StatusCode(500, "Internal Server Error");
+        }
+    }
 }
