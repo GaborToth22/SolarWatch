191365117ba37b0206e198da438aeccc3f963143
GaborToth22 <toccs22@gmail.com>
2024-02-20 10:11:30 +0100

    feat: add homePage with functionality

--- Frontend/SolarWatch/src/Components/HomePage.jsx
+++ Frontend/SolarWatch/src/Components/HomePage.jsx
@@ -1,17 +1,82 @@
-import { useState, useEffect } from "react";
+import { useState, } from "react";
+import {  Link } from "react-router-dom";
 
-function HomePage() {
-    const [logged, setlogged] = useState(false);
-    const [adsData, setAdsData] = useState([]);
+function HomePage({ loggedUser, setLoggedUser}) {
+    const[cityName, setCityName] = useState("");
+    const[date, setDate] = useState("");
+    const[solarData, setSolarData] = useState("");
         
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch(`/api/SolarWatch?cityName=${cityName}&date=${date}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${loggedUser.token}`
+                },
+            });
+            if (!response.ok) {
+                throw new Error('Failed to fetch data');
+            }
+            const data = await response.json();
+            console.log(data)
+            setSolarData(data);
+            
+        } catch (error) {
+            console.error("invalid city or date")
+        }
+    }
+    
     return (
-        <>
-            <div className="homepage-container">                    
+        <div className="background-container">
+            { loggedUser ? 
+            (solarData === "" ?
+            (<div className="homepage-container">                    
                 <div className="content-area">
-                        Sanyi
+                    <h2 className="title">SolarWatch</h2>
+                    <form>
+                    <input type="text" placeholder="City Name" value={cityName} onChange={(e) => setCityName(e.target.value)}/> <br />
+                    <input type="date" placeholder="date" value={date} onChange={(e) => setDate(e.target.value)}/> <br />
+                    <button className="button" onClick={handleSubmit}>Submit</button>
+                    </form>
+                </div>
+            </div>) 
+            :
+            (<div className="homepage-container">                    
+                <div className="content-area">
+                   <h2 className="title">SolarWatch</h2>
+                   <form>
+                   <input type="text" placeholder="City Name" value={cityName} onChange={(e) => setCityName(e.target.value)}/> <br />
+                   <input type="date" placeholder="date" value={date} onChange={(e) => setDate(e.target.value)}/> <br />
+                    <button className="button" onClick={handleSubmit}>Submit</button>
+                    </form>
+                </div>
+                <div className="solarData">
+                    <h2>{cityName}</h2>
+                    <h3>Date: {solarData.date.slice(0, 10)}</h3>
+                    <div className="sunrise-info">
+                        <p>Sunrise: {solarData.sunRise}</p>
+                    </div>
+                    <div className="sunset-info">
+                        <p>Sunset: {solarData.sunSet}</p>
+                    </div>
+                </div>
+            </div>)
+            ) 
+            :
+            (<div className="homepage-container">
+                <div className="content-area">
+                    <p>You must log in to view this page</p>
+                    <Link to="/login">
+                        <button className="button">Login</button>
+                    </Link>
                 </div>
             </div>
-        </>
+            )
+            }
+        </div>
     );
 }
 
